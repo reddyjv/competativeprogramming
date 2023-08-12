2d05c305963dd5d645e0231c5239fdfa11008049
Aravind Reddy J V <aravindjv.is21@sahyadri.edu.in>
2023-08-12 17:26:52 +0000

    commit

---
+++ .vscode/standardtemplates/vector.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    vector<int> a(5,1);
+    vector<int> last(a);
+    cout<<"print last"<<endl;
+    for(int i:last){
+        cout<<i<<" ";
+     }cout<<endl;
+     cout<<"capacity->"<<v.capacity()<<endl;
+     v.push_back(1);
+     cout<<"capacity->"<<v.capacity()<<endl;
+     v.push_back(2);
+      cout<<"capacity->"<<v.capacity()<<endl;
+         v.push_back(3);
+      cout<<"capacity->"<<v.capacity()<<endl;
+      cout<<"size->"<<v.size()<<endl;
+      cout<<"element at index at 2"<<v.at(2)<<endl;
+      cout<<"front"<<v.front()<<endl;
+      cout<<"back"<<v.back()<<endl;
+      cout<<"before pop"<<endl;
+      for(int i:v){
+        cout<<i<<" ";
+      }cout<<endl;
+      v.pop_back();
+      v.pop_back();
+         cout<<"after pop"<<endl;
+      for(int i:v){
+        cout<<i<<" ";
+      }cout<<endl;
+
+
+
+
+
+}
