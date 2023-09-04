2ad32d80fa2af712803f71c4e65877301492fa91
Aravind Reddy J V <aravindjv.is21@sahyadri.edu.in>
2023-09-04 12:44:55 +0000

    commit

---
+++ Arrays/findingpivot.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int peakindex(int array[],int m){
+    int s=0;
+    int e=m-1;
+    int mid=s+(e-s)/2;
+    while(s<e){
+        if(array[mid]>=array[mid+1]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+        mid=s+(e-s)/2;
+    }
+    return s;
+
+}
+int main(){
+       int a[100],n,key;
+      cout<<"enter number of elements"<<endl;
+    cin>>n;
+    cout<<"enter number of elements"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+
+    }
+    cout<<"the pivot is in index  "<<peakindex(a,n)<<endl;
+
+
+}
+/*output
+
+enter number of elements
+5
+enter number of elements
+8 10 17 1 3
+the pivot is in index  3
+*/
