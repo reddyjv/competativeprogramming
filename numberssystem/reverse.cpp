7d346a20a9051d14bec31e2a3b74635328c6ff37
Aravind Reddy J V <aravindjv.is21@sahyadri.edu.in>
2023-10-26 08:56:10 +0000

    commit

--- numberssystem/reverse.cpp
+++ numberssystem/reverse.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello word"<<endl;
+   int n,rev=0;
+   cout<<"enter the number"<<endl;
+   cin>>n;
+   while(n>0)
+   {
+    int rem=n%10;
+    n=n/10;
+    rev=rev*10+rem;
+
+   }
+   cout<<"the reversed number is "<<rev<<endl;
 }
